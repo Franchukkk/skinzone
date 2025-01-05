0e19a56b45dad70c780cc6ced385eb1541b0f470
Franchukkk <sha471251@gmail.com>
2025-01-05 12:48:16 +0200

    slider

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -23,3 +23,40 @@ document.addEventListener("scroll", () => {
         line.style.transform = `translateX(${newOffset - 320}px)`; // Початковий зсув -300px
     });
 });
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+const discountsSlider = new Swiper('.discounts-slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        }
+    }
+});   
