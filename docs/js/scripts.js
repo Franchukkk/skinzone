38c67c867e636b2b8c8fa0682cb5ad4f0f6a04b4
Franchukkk <sha471251@gmail.com>
2025-01-07 18:03:09 +0200

    circles

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,3 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector("body");
+    const circles = Array.from(container.querySelectorAll(".circle"));
+    const screenWidth = window.innerWidth;
+    const screenHeight = document.innerHeight;
+  
+    function getRandom(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+  
+    function createNewCircle() {
+      const circle = document.createElement("div");
+      circle.className = "circle";
+      container.appendChild(circle);
+      circles.push(circle);
+      const size = getRandom(10, 50);
+      circle.style.width = `${size}px`;
+      circle.style.height = `${size}px`;
+      circle.style.left = `${getRandom(0, screenWidth - size)}px`;
+      circle.style.top = `${window.scrollY}px`;
+      circle.speed = getRandom(1, 3);
+    }
+  
+    function resetCirclePosition(circle) {
+      const size = getRandom(10, 50);
+      circle.style.width = `${size}px`;
+      circle.style.height = `${size}px`;
+      circle.style.left = `${getRandom(0, screenWidth - size)}px`;
+      circle.style.top = `-${size}px`;
+      circle.speed = getRandom(1, 3);
+    }
+  
+    function updateCirclePosition(circle) {
+      const top = parseFloat(circle.style.top);
+      circle.style.top = `${top + circle.speed}px`;
+  
+      if (top > screenHeight) {
+        resetCirclePosition(circle);
+      }
+    }
+  
+    function initCircles() {
+      circles.forEach((circle) => {
+        resetCirclePosition(circle);
+      });
+    }
+  
+    function animate() {
+      circles.forEach((circle) => {
+        updateCirclePosition(circle);
+      });
+      requestAnimationFrame(animate);
+    }
+  
+    initCircles();
+    animate();
+    
+    setInterval(createNewCircle, 10000);
+  });
 document.addEventListener("scroll", () => {
     const lines = document.querySelectorAll(".marquee-line");
     const scrollTop = window.scrollY;
